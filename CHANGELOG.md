4061532bdb64cb38983db58f07560de262882c47
cesarromerodev <cesar_romero.dev@outlook.com>
2022-06-15 13:11:50 -0500

    restServer

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# RESTServer
+
+## Inicio del Proyecto
+
+    - npm init -y
+
+## Instalaciones:
+
+    - express : Para crear nuestro servidor     https://www.npmjs.com/package/express
+    - dotenv  : Configurar variables de entorno     https://www.npmjs.com/package/dotenv
+      Comando: npm i express dotenv
+    - Cors    : Nos permite protejer nuestro servidor
---
+++ models/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+        //Middlewares
+        this.middlewares();
+        //Rutas Aplicacion
+        this.routes();
+    }
+    middlewares(){
+        this.app.use( cors() )                           //CORS
+        this.app.use( express.json() );                    //Lectura y parseo del body
+        this.app.use( express.static('public') );        //Directorio Publico
+    }
+
+    routes(){
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen(){
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        } );
+    }
+}
+
+
+module.exports = Server;    //exportar servidor
